66fdf78b460b295978f563c7a1bdd3d9ec4e98ac
Flávio Eduardo <xflax00@gmail.com>
2024-04-07 13:07:11 -0300

    refactor: adapting to clean architecture

--- FinTracker.API/Program.cs
+++ FinTracker.API/Program.cs
@@ -1,6 +1,6 @@
-using FinTracker.API.Models;
-using FinTracker.API.Validators;
+using FinTranker.Infrastructure.Database;
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +12,9 @@ builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
---
+++ FinTranker.Domain/Entities/Finance.cs
@@ -0,0 +1,16 @@
+﻿namespace FinTranker.Domain.Entities;
+
+public class Finance
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Category { get; set; }
+
+    public string Reason { get; set; }
+
+    public decimal Value { get; set; }
+
+    public DateTime OcurrencyDate { get; set; }
+}
